962bebd4ac88d1e5ca218900613fb0711ad1d486
SteamShon <shom83@gmail.com>
2015-06-09 14:28:04 +0900

    passed EdgeTest part1.

--- s2core/src/test/scala/com/daumkakao/s2graph/core/EdgeTest.scala
+++ s2core/src/test/scala/com/daumkakao/s2graph/core/EdgeTest.scala
@@ -2,7 +2,7 @@ package com.daumkakao.s2graph.core
 
 import com.daumkakao.s2graph.core.Edge.PropsPairWithTs
 import com.daumkakao.s2graph.core.models.LabelMeta
-import com.daumkakao.s2graph.core.types2.CompositeId
+import com.daumkakao.s2graph.core.types2.{InnerVal, InnerValLikeWithTs, CompositeId}
 import org.hbase.async.{AtomicIncrementRequest, PutRequest}
 import org.scalatest.{BeforeAndAfter, Matchers, FunSuite}
 
@@ -30,121 +30,161 @@ class EdgeTest extends FunSuite with Matchers with TestCommon with TestCommonWit
     }
   }
 
-  test("insert for edgesWithIndex version 1") {
+  //  test("insert for edgesWithIndex version 1") {
+  //    val rets = for {
+  //      edgeForSameTgtVertex <- testEdges
+  //    } yield {
+  //        val head = edgeForSameTgtVertex.head
+  //        val start = head
+  //        var prev = head
+  //        val rets = for {
+  //          edge <- edgeForSameTgtVertex.tail
+  //        } yield {
+  //            val rets = for {
+  //              edgeWithIndex <- edge.edgesWithIndex
+  //            } yield {
+  //                val prevPuts = prev.edgesWithIndex.flatMap { prevEdgeWithIndex =>
+  //                  prevEdgeWithIndex.buildPutsAsync().map { rpc => rpc.asInstanceOf[PutRequest] }
+  //                }
+  //                val puts = edgeWithIndex.buildPutsAsync().map { rpc =>
+  //                  rpc.asInstanceOf[PutRequest]
+  //                }
+  //                val comps = for {
+  //                  put <- puts
+  //                  prevPut <- prevPuts
+  //                } yield largerThan(put.qualifier(), prevPut.qualifier())
+  //
+  //                val rets = for {
+  //                  put <- puts
+  //                  decodedEdge <- Edge.toEdge(putToKeyValue(put), queryParam)
+  //                } yield edge == decodedEdge
+  //
+  //                rets.forall(x => x) && comps.forall(x => x)
+  //              }
+  //
+  //            prev = edge
+  //            rets.forall(x => x)
+  //          }
+  //        rets.forall(x => x)
+  //      }
+  //  }
+  //
+  //  test("insert for edgeWithInvertedIndex") {
+  //    val rets = for {
+  //      edgeForSameTgtVertex <- testEdges
+  //    } yield {
+  //        val head = edgeForSameTgtVertex.head
+  //        val start = head
+  //        var prev = head
+  //        val rets = for {
+  //          edge <- edgeForSameTgtVertex.tail
+  //        } yield {
+  //            val ret = {
+  //              val edgeWithInvertedIndex = edge.edgesWithInvertedIndex
+  //              val prevPut = prev.edgesWithInvertedIndex.buildPutAsync()
+  //              val put = edgeWithInvertedIndex.buildPutAsync()
+  //              val comp = largerThan(put.qualifier(), prevPut.qualifier())
+  //              val decodedEdge = Edge.toEdge(putToKeyValue(put), queryParam)
+  //              comp && decodedEdge == edge
+  //            }
+  //            ret
+  //          }
+  //        rets.forall(x => x)
+  //      }
+  //  }
+  import InnerVal.{VERSION1, VERSION2}
+  def testPropsUpdate(oldProps: Map[Byte, InnerValLikeWithTs],
+                      newProps: Map[Byte, InnerValLikeWithTs],
+                      expected: Map[Byte, Any],
+                      expectedShouldUpdate: Boolean)
+                     (f: PropsPairWithTs => (Map[Byte, InnerValLikeWithTs], Boolean))(version: String) = {
+    val (updated, shouldUpdate) = f((oldProps, newProps, ts + 1, version))
     val rets = for {
-      edgeForSameTgtVertex <- testEdges
+      (k, v) <- expected
     } yield {
-        val head = edgeForSameTgtVertex.head
-        val start = head
-        var prev = head
-        val rets = for {
-          edge <- edgeForSameTgtVertex.tail
-        } yield {
-            val rets = for {
-              edgeWithIndex <- edge.edgesWithIndex
-            } yield {
-                val prevPuts = prev.edgesWithIndex.flatMap { prevEdgeWithIndex =>
-                  prevEdgeWithIndex.buildPutsAsync().map { rpc => rpc.asInstanceOf[PutRequest] }
-                }
-                val puts = edgeWithIndex.buildPutsAsync().map { rpc =>
-                  rpc.asInstanceOf[PutRequest]
-                }
-                val comps = for {
-                  put <- puts
-                  prevPut <- prevPuts
-                } yield largerThan(put.qualifier(), prevPut.qualifier())
-
-                val rets = for {
-                  put <- puts
-                  decodedEdge <- Edge.toEdge(putToKeyValue(put), queryParam)
-                } yield edge == decodedEdge
-
-                rets.forall(x => x) && comps.forall(x => x)
-              }
-
-            prev = edge
-            rets.forall(x => x)
-          }
-        rets.forall(x => x)
+        v match {
+          case v: String =>
+            v match {
+              case "left" => updated.get(k).isDefined && updated(k) == oldProps(k)
+              case "right" => updated.get(k).isDefined && updated(k) == newProps(k)
+              case "none" => updated.get(k).isEmpty
+            }
+          case value: InnerValLikeWithTs => updated.get(k).get == value
+          case _ => throw new RuntimeException(s"not supported keyword: $v")
+        }
       }
+
+    rets.forall(x => x) && shouldUpdate == expectedShouldUpdate
   }
-//
-//  test("insert for edgeWithInvertedIndex") {
-//    val rets = for {
-//      edgeForSameTgtVertex <- testEdges
-//    } yield {
-//        val head = edgeForSameTgtVertex.head
-//        val start = head
-//        var prev = head
-//        val rets = for {
-//          edge <- edgeForSameTgtVertex.tail
-//        } yield {
-//            val ret = {
-//              val edgeWithInvertedIndex = edge.edgesWithInvertedIndex
-//              val prevPut = prev.edgesWithInvertedIndex.buildPutAsync()
-//              val put = edgeWithInvertedIndex.buildPutAsync()
-//              val comp = largerThan(put.qualifier(), prevPut.qualifier())
-//              val decodedEdge = Edge.toEdge(putToKeyValue(put), queryParam)
-//              comp && decodedEdge == edge
-//            }
-//            ret
-//          }
-//        rets.forall(x => x)
-//      }
-//  }
-//  def testPropsUpdate(oldProps: Map[Byte, InnerValWithTs],
-//                      newProps: Map[Byte, InnerValWithTs],
-//                      expected: Map[Byte, String],
-//                      expectedShouldUpdate: Boolean)(f: PropsPairWithTs => (Map[Byte, InnerValWithTs], Boolean)) = {
-//    val (updated, shouldUpdate) = f((oldProps, newProps, ts + 1))
-//    val rets = for {
-//      (k, v) <- expected
-//    } yield {
-//      v match {
-//        case "left" => updated.get(k).isDefined && updated(k) == oldProps(k)
-//        case "right" => updated.get(k).isDefined && updated(k) == newProps(k)
-//        case "none" => updated.get(k).isEmpty
-//        case _ => throw new RuntimeException(s"not supported keyword: $v")
-//      }
-//    }
-//    rets.forall(x => x) && shouldUpdate == expectedShouldUpdate
-//  }
-//  /** test cases for each operation */
-//  val oldProps = Map(
-//    LabelMeta.lastDeletedAt -> InnerValWithTs.withLong(ts -2, ts -2),
-//    1.toByte -> InnerValWithTs.withLong(0L, ts),
-//    2.toByte -> InnerValWithTs.withLong(1L, ts - 1),
-//    4.toByte -> InnerValWithTs.withStr("old", ts - 1)
-//  )
-//  val newProps = Map(
-//      2.toByte -> InnerValWithTs.withLong(-10L, ts + 1),
-//      3.toByte -> InnerValWithTs.withLong(20L, ts + 1)
-//    )
-//  test("Edge.buildUpsert") {
-//    val shouldUpdate = true
-//    val expected =  Map(
-//      LabelMeta.lastDeletedAt -> "left",
-//      1.toByte -> "none",
-//      2.toByte -> "right",
-//      3.toByte -> "right",
-//      4.toByte -> "none")
-//    testPropsUpdate(oldProps, newProps, expected, shouldUpdate)(Edge.buildUpsert) shouldBe true
-//  }
-//  test("Edge.buildUpdate") {
-//    val shouldUpdate = true
-//    val expected = Map(
-//      LabelMeta.lastDeletedAt -> "left",
-//      1.toByte -> "left",
-//      2.toByte -> "right",
-//      3.toByte -> "right",
-//      4.toByte -> "left"
-//    )
-//    testPropsUpdate(oldProps, newProps, expected, shouldUpdate)(Edge.buildUpdate) shouldBe true
-//  }
-//  test("Edge.buildDelete") {
-//    val shouldUpdate = true
-//    val expected = Map(
-//
-//    )
-//  }
+
+  //  /** test cases for each operation */
+  def oldProps(version: String) = {
+    Map(
+      LabelMeta.lastDeletedAt -> InnerValLikeWithTs.withLong(ts - 2, ts - 2, version),
+      1.toByte -> InnerValLikeWithTs.withLong(0L, ts, version),
+      2.toByte -> InnerValLikeWithTs.withLong(1L, ts - 1, version),
+      4.toByte -> InnerValLikeWithTs.withStr("old", ts - 1, version)
+    )
+  }
+
+  def newProps(version: String) = {
+    Map(
+      2.toByte -> InnerValLikeWithTs.withLong(-10L, ts + 1, version),
+      3.toByte -> InnerValLikeWithTs.withLong(20L, ts + 1, version)
+    )
+  }
+
+  def deleteProps(timestamp: Long, version: String) = Map(
+    LabelMeta.lastDeletedAt -> InnerValLikeWithTs.withLong(timestamp, timestamp, version)
+  )
+
+  test("Edge.buildUpsert") {
+    val shouldUpdate = true
+    val oldState = oldProps(VERSION2)
+    val newState = newProps(VERSION2)
+    val expected = Map(
+      LabelMeta.lastDeletedAt -> "left",
+      1.toByte -> "none",
+      2.toByte -> "right",
+      3.toByte -> "right",
+      4.toByte -> "none")
+     testPropsUpdate(oldState, newState, expected, shouldUpdate)(Edge.buildUpsert)(VERSION2) shouldBe true
+  }
+  test("Edge.buildUpdate") {
+    val shouldUpdate = true
+    val oldState = oldProps(VERSION2)
+    val newState = newProps(VERSION2)
+    val expected = Map(
+      LabelMeta.lastDeletedAt -> "left",
+      1.toByte -> "left",
+      2.toByte -> "right",
+      3.toByte -> "right",
+      4.toByte -> "left"
+    )
+     testPropsUpdate(oldState, newState, expected, shouldUpdate)(Edge.buildUpdate)(VERSION2) shouldBe true
+  }
+  test("Edge.buildDelete") {
+    val shouldUpdate = true
+    val oldState = oldProps(VERSION2)
+    val newState = deleteProps(ts + 1, VERSION2)
+    val expected = Map(
+      LabelMeta.lastDeletedAt -> "right",
+      1.toByte -> "none",
+      2.toByte -> "none",
+      4.toByte -> "none"
+    )
+    testPropsUpdate(oldState, newState, expected, shouldUpdate)(Edge.buildDelete)(VERSION2) shouldBe true
+  }
+  test("Edge.buildIncrement") {
+    val shouldUpdate = true
+    val oldState = oldProps(VERSION2).filterNot(kv => kv._1 == 4.toByte)
+    val newState = newProps(VERSION2)
+    val expected = Map(
+      LabelMeta.lastDeletedAt -> "left",
+      1.toByte -> "left",
+      2.toByte -> InnerValLikeWithTs.withLong(-9L, ts - 1, VERSION2),
+      3.toByte -> "right"
+    )
+    testPropsUpdate(oldState, newState, expected, shouldUpdate)(Edge.buildIncrement)(VERSION2) shouldBe true
+  }
 }
