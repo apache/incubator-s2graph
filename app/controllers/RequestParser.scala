02b7c69876c2a186980f59abbae57e95381a99ff
daewon <blueiur@gmail.com>
2015-07-03 13:41:42 +0900

    fix error on parsing createLabel without indexProps/Props

--- app/controllers/RequestParser.scala
+++ app/controllers/RequestParser.scala
@@ -164,7 +164,7 @@ trait RequestParser extends JSONParser {
               val includeDegree = (labelGroup \ "includeDegree").asOpt[Boolean].getOrElse(true)
               val rpcTimeout = (labelGroup \ "rpcTimeout").asOpt[Int].getOrElse(1000)
               val maxAttempt = (labelGroup \ "maxAttempt").asOpt[Int].getOrElse(2)
-              val tgtVertexInnerIdOpt = (labelGroup \ "_to").asOpt[JsValue].flatMap { jsVal =>
+              val tgtVertexInnerIdOpt = (labelGroup \ "includeDegree").asOpt[JsValue].flatMap { jsVal =>
                 jsValueToInnerVal(jsVal, label.tgtColumnWithDir(direction).columnType, label.schemaVersion)
               }
               val cacheTTL = (labelGroup \ "cacheTTL").asOpt[Long].getOrElse(-1L)
@@ -271,7 +271,7 @@ trait RequestParser extends JSONParser {
 
   def parsePropsElements(jsValue: JsValue) = {
     for {
-      jsObj <- jsValue.as[List[JsValue]]
+      jsObj <- jsValue.asOpt[List[JsValue]].getOrElse(Nil)
     } yield {
       val propName = (jsObj \ "name").as[String]
       val dataType = InnerVal.toInnerDataType((jsObj \ "dataType").as[String])
