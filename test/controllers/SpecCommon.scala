1fb83391732e7578118333d5569c31499001c04b
daewon <blueiur@gmail.com>
2015-08-17 11:30:42 +0900

    fix the bug: apply config: async.hbase.client.flush.interval

--- test/controllers/SpecCommon.scala
+++ test/controllers/SpecCommon.scala
@@ -36,7 +36,7 @@ trait SpecCommon extends Specification {
   protected val testTgtColumnName = "item_id_test"
   val NUM_OF_EACH_TEST = 3
   lazy val HTTP_REQ_WAITING_TIME = Duration(5000, MILLISECONDS)
-  val asyncFlushInterval = 1000
+  val asyncFlushInterval = 300
 
   val createService = s"""{"serviceName" : "$testServiceName"}"""
   val testLabelNameCreate = s"""
