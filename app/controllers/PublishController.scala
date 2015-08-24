c921b6a7a5cab8e612ae7dd8e847f275adf32d43
SteamShon <shom83@gmail.com>
2015-08-24 18:35:22 +0900

    change kafka topic name should be registered as service.

--- app/controllers/PublishController.scala
+++ app/controllers/PublishController.scala
@@ -1,7 +1,9 @@
 package controllers
 
 import com.daumkakao.s2graph.core.ExceptionHandler
+import com.daumkakao.s2graph.core.mysqls.Service
 import config.Config
+import play.api.Logger
 import play.api.mvc._
 import scala.concurrent.Future
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -14,17 +16,29 @@ object PublishController extends Controller {
   /**
    * never check validation on string. just redirect strings to kafka.
    */
-  def publishOnly(service: String) = withHeaderAsync(parse.text) { request =>
+  val serviceNotExistException = new RuntimeException(s"service is not created in s2graph. create service first.")
+
+  private def toService(topic: String): String = {
+    Service.findByName(topic).map(service => s"${service.serviceName}-${Config.PHASE}").getOrElse(throw serviceNotExistException)
+  }
+  def publishOnly(topic: String) = withHeaderAsync(parse.text) { request =>
     if (!Config.IS_WRITE_SERVER) Future.successful(UNAUTHORIZED)
 
-    val strs = request.body.split("\n")
-    strs.foreach(str => {
-      val keyedMessage = new ProducerRecord[Key, Val](Config.KAFKA_LOG_TOPIC, s"$service\t$str")
-      ExceptionHandler.enqueue(KafkaMessage(keyedMessage))
-    })
-    Future.successful(
-      Ok("publish success.\n").withHeaders(CONNECTION -> "Keep-Alive", "Keep-Alive" -> "timeout=10, max=10")
-    )
+    try {
+      val kafkaTopic = toService(topic)
+      val strs = request.body.split("\n")
+      strs.foreach(str => {
+//        val keyedMessage = new ProducerRecord[Key, Val](Config.KAFKA_LOG_TOPIC, s"$str")
+        val keyedMessage = new ProducerRecord[Key, Val](kafkaTopic, s"$str")
+//        Logger.debug(s"$kafkaTopic, $str")
+        ExceptionHandler.enqueue(KafkaMessage(keyedMessage))
+      })
+      Future.successful(
+        Ok("publish success.\n").withHeaders(CONNECTION -> "Keep-Alive", "Keep-Alive" -> "timeout=10, max=10")
+      )
+    } catch {
+      case e: Exception => Future.successful(BadRequest(e.getMessage))
+    }
   }
 
   def publish(topic: String) = publishOnly(topic)
