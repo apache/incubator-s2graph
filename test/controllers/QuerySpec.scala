0d59ea57318a4eb78229e7b1a4ca743f630b73a7
daewon <blueiur@gmail.com>
2015-09-10 15:31:09 +0900

    add test

--- test/controllers/QuerySpec.scala
+++ test/controllers/QuerySpec.scala
@@ -1,7 +1,5 @@
 package test.controllers
 
-import com.daumkakao.s2graph
-import com.daumkakao.s2graph.logger
 import play.api.libs.json._
 import play.api.test.{FakeApplication, FakeRequest, PlaySpecification}
 import play.api.{Application => PlayApplication}
@@ -9,12 +7,16 @@ import play.api.{Application => PlayApplication}
 import scala.concurrent.Await
 
 class QuerySpec extends SpecCommon with PlaySpecification {
-  init()
 
   import Helper._
 
+  implicit val app = FakeApplication()
+
+  init()
+
   "query test" should {
     running(FakeApplication()) {
+
       // insert bulk and wait ..
       val bulkEdges: String = Seq(
         edge"1000 insert e 0 1 $testLabelName"($(weight = 40, is_hidden = true)),
@@ -22,8 +24,19 @@ class QuerySpec extends SpecCommon with PlaySpecification {
         edge"3000 insert e 2 0 $testLabelName"($(weight = 20)),
         edge"4000 insert e 2 1 $testLabelName"($(weight = 10)),
         edge"3000 insert e 10 20 $testLabelName"($(weight = 20)),
-        edge"4000 insert e 20 20 $testLabelName"($(weight = 10))
-      ).mkString("\n")
+        edge"4000 insert e 20 20 $testLabelName"($(weight = 10)),
+        edge"1 insert e -1 1000 $testLabelName",
+        edge"1 insert e -1 2000 $testLabelName",
+        edge"1 insert e -1 3000 $testLabelName",
+        edge"1 insert e 1000 10000 $testLabelName",
+        edge"1 insert e 1000 11000 $testLabelName",
+        edge"1 insert e 2000 11000 $testLabelName",
+        edge"1 insert e 2000 12000 $testLabelName",
+        edge"1 insert e 3000 12000 $testLabelName",
+        edge"1 insert e 3000 13000 $testLabelName",
+        edge"1 insert e 10000 100000 $testLabelName",
+        edge"2 insert e 11000 200000 $testLabelName",
+        edge"3 insert e 12000 300000 $testLabelName").mkString("\n")
 
       val req = FakeRequest(POST, "/graphs/edges/bulk").withBody(bulkEdges)
       Await.result(route(req).get, HTTP_REQ_WAITING_TIME)
@@ -92,8 +105,55 @@ class QuerySpec extends SpecCommon with PlaySpecification {
       contentAsJson(ret)
     }
 
+    def queryIndex(ids: Seq[Int], indexName: String) = {
+      val $from = $a(
+        $(serviceName = testServiceName,
+          columnName = testColumnName,
+          ids = ids))
 
-   "get edge with where condition" in {
+      val $step = $a($(label = testLabelName, index = indexName))
+      val $steps = $a($(step = $step))
+
+      val js = $(withScore = false, srcVertices = $from, steps = $steps).toJson
+      js
+    }
+
+    def queryDuration(ids: Seq[Int], from: Int, to: Int) = {
+      val $from = $a(
+        $(serviceName = testServiceName,
+          columnName = testColumnName,
+          ids = ids))
+
+      val $step = $a($(
+        label = testLabelName, direction = "out", offset = 0, limit = 100,
+        duration = $(from = from, to = to)))
+
+      val $steps = $a($(step = $step))
+
+      $(srcVertices = $from, steps = $steps).toJson
+    }
+
+    def queryAncestor(ids: Seq[Int], ancestorAt: Int) = {
+      val $from = $a(
+        $(serviceName = testServiceName,
+          columnName = testColumnName,
+          ids = ids))
+
+      val $step = $a($(label = testLabelName, direction = "out", offset = 0, limit = 100))
+
+      val $steps =
+        if (ancestorAt == 1) {
+          $a($(step = $step, shouldPropagate = true), $(step = $step), $(step = $step))
+        } else {
+          $a($(step = $step), $(step = $step, shouldPropagate = true), $(step = $step))
+        }
+
+      $(srcVertices = $from, steps = $steps).toJson
+    }
+
+
+
+    "get edge with where condition" in {
       running(FakeApplication()) {
         var result = getEdges(queryWhere(0, "is_hidden=false and _from in (-1, 0)"))
         (result \ "results").as[List[JsValue]].size must equalTo(1)
@@ -133,19 +193,6 @@ class QuerySpec extends SpecCommon with PlaySpecification {
       }
     }
 
-    def queryIndex(ids: Seq[Int], indexName: String) = {
-      val $from = $a(
-        $(serviceName = testServiceName,
-          columnName = testColumnName,
-          ids = ids))
-
-      val $step = $a($(label = testLabelName, index = indexName))
-      val $steps = $a($(step = $step))
-
-      val js = $(withScore = false, srcVertices = $from, steps = $steps).toJson
-      js
-    }
-
     "index" in {
       running(FakeApplication()) {
         // weight order
@@ -156,21 +203,6 @@ class QuerySpec extends SpecCommon with PlaySpecification {
         result = getEdges(queryIndex(Seq(0), "idx_2"))
         ((result \ "results").as[List[JsValue]].head \\ "weight").head must equalTo(JsNumber(30))
       }
-    }
-
-    def queryDuration(ids: Seq[Int], from: Int, to: Int) = {
-      val $from = $a(
-        $(serviceName = testServiceName,
-          columnName = testColumnName,
-          ids = ids))
-
-      val $step = $a($(
-        label = testLabelName, direction = "out", offset = 0, limit = 100,
-        duration = $(from = from, to = to)))
-
-      val $steps = $a($(step = $step))
-
-      $(srcVertices = $from, steps = $steps).toJson
     }
 
     "duration" in {
@@ -212,5 +244,19 @@ class QuerySpec extends SpecCommon with PlaySpecification {
       }
     }
 
+    "ancestor" in {
+      running(FakeApplication()) {
+        var result = getEdges(queryAncestor(Seq(-1), 1))
+        var expect = Seq(Set("2000", "3000"), Set("1000", "2000"), Set("1000"))
+        var ancestors = (result \\ "ancestor").map(_.as[Seq[String]]).map(_.toSet)
+        ancestors must equalTo(expect)
+
+        result = getEdges(queryAncestor(Seq(-1), 2))
+        expect = Seq(Set("12000"), Set("11000"), Set("10000"))
+        ancestors = (result \\ "ancestor").map(_.as[Seq[String]]).map(_.toSet)
+        ancestors must equalTo(expect)
+        true
+      }
+    }
   }
 }
