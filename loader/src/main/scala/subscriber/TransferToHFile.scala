23e82186976dc02fc0c548b3af10157c9a63dc8d
SteamShon <shom83@gmail.com>
2015-09-24 18:27:01 +0900

    refactor hBulk

--- loader/src/main/scala/subscriber/TransferToHFile.scala
+++ loader/src/main/scala/subscriber/TransferToHFile.scala
@@ -4,6 +4,8 @@ import java.util.TreeSet
 
 import com.daumkakao.s2graph.core.{Edge, Graph}
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.fs.permission.FsPermission
 import org.apache.hadoop.hbase.client.{HTable, ConnectionFactory}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.{LoadIncrementalHFiles, HFileOutputFormat2, TableOutputFormat}
@@ -17,8 +19,7 @@ import org.hbase.async.{PutRequest}
 import s2.spark.{HashMapParam, SparkApp}
 
 import scala.collection.mutable.{HashMap => MutableHashMap}
-import unicredit.spark.hbase._
-
+import scala.collection.JavaConversions.asScalaSet
 import scala.util.Random
 
 object TransferToHFile extends SparkApp {
@@ -37,23 +38,24 @@ object TransferToHFile extends SparkApp {
      """.stripMargin
 
   //
-  //  private class HFilePartitioner(conf: Configuration, splits: Array[Array[Byte]], numFilesPerRegion: Int) extends Partitioner {
-  //    val fraction = 1 max numFilesPerRegion min conf.getInt(LoadIncrementalHFiles.MAX_FILES_PER_REGION_PER_FAMILY, 32)
-  //
-  //    override def getPartition(key: Any): Int = {
-  //      def bytes(n: Any) = n match {
-  //        case s: String => Bytes.toBytes(s)
-  //      }
-  //
-  //      val h = (key.hashCode() & Int.MaxValue) % fraction
-  //      for (i <- 1 until splits.length)
-  //        if (Bytes.compareTo(bytes(key), splits(i)) < 0) return (i - 1) * fraction + h
-  //
-  //      (splits.length - 1) * fraction + h
-  //    }
-  //
-  //    override def numPartitions: Int = splits.length * fraction
-  //  }
+  private class HFilePartitioner(conf: Configuration, splits: Array[Array[Byte]], numFilesPerRegion: Int) extends Partitioner {
+    val fraction = 1 max numFilesPerRegion min conf.getInt(LoadIncrementalHFiles.MAX_FILES_PER_REGION_PER_FAMILY, 32)
+
+    override def getPartition(key: Any): Int = {
+      def bytes(n: Any) = n match {
+        case s: String => Bytes.toBytes(s)
+        case b: ImmutableBytesWritable => b.get()
+      }
+
+      val h = (key.hashCode() & Int.MaxValue) % fraction
+      for (i <- 1 until splits.length)
+        if (Bytes.compareTo(bytes(key), splits(i)) < 0) return (i - 1) * fraction + h
+
+      (splits.length - 1) * fraction + h
+    }
+
+    override def numPartitions: Int = splits.length * fraction
+  }
 
   /*
       1270054885000	insertBulk	e	85418	97556	talk_friend	{"created_at": 1269799161000}
@@ -79,29 +81,54 @@ object TransferToHFile extends SparkApp {
     kvs.toIterator
   }
 
-  def buildCells(rdd: RDD[String], dbUrl: String, numOfHFiles: Int) = {
+  def buildCells(rdd: RDD[String], dbUrl: String, conf: Configuration, hTable: HTable, numFilesPerRegion: Int) = {
     val kvs = rdd.mapPartitions { iter =>
 
       val phase = System.getProperty("phase")
       GraphSubscriberHelper.apply(phase, dbUrl, "none", "none")
+
       toKeyValues(iter.toSeq)
-
     }
-
-    implicit val ordering = new Ordering[KeyValue] {
-      override def compare(a: KeyValue, b: KeyValue) = {
-        KeyValue.COMPARATOR.compare(a, b)
+    implicit val bytesOrdering = new Ordering[ImmutableBytesWritable] {
+      override def compare(a: ImmutableBytesWritable, b: ImmutableBytesWritable) = {
+        Bytes.compareTo(a.get(), b.get())
       }
     }
-
-    kvs.repartition(numOfHFiles).sortBy { kv =>
-      kv
-    }.map { kv =>
+    val grouped = kvs.groupBy { kv =>
       val hKey = new ImmutableBytesWritable(kv.getRow())
-//      println(s"[Key]: ${hKey.get().toList}")
-//      println(s"[Value]: ${kv}")
-      (hKey -> kv)
+      hKey
     }
+    val sorted = grouped.repartitionAndSortWithinPartitions(new HFilePartitioner(conf, hTable.getStartKeys, numFilesPerRegion))
+    val ret = sorted.flatMap { case (hKey, kvs) =>
+        val inner = new TreeSet[KeyValue](KeyValue.COMPARATOR)
+        for {
+          kv <- kvs
+        } {
+          inner.add(kv)
+        }
+        inner.toSeq map { kv =>
+//          println(s"[Key]: ${hKey.get.toList}")
+//          println(s"[KeyValue]: ${kv}")
+          (hKey -> kv)
+        }
+
+    }
+    ret
+    //
+    //    implicit val ordering = new Ordering[KeyValue] {
+    //      override def compare(a: KeyValue, b: KeyValue) = {
+    //        KeyValue.COMPARATOR.compare(a, b)
+    //      }
+    //    }
+    //
+    //    kvs.repartition(numOfHFiles).sortBy { kv =>
+    //      kv
+    //    }.map { kv =>
+    //      val hKey = new ImmutableBytesWritable(kv.getRow())
+    ////      println(s"[Key]: ${hKey.get().toList}")
+    ////      println(s"[Value]: ${kv}")
+    //      (hKey -> kv)
+    //    }
 
   }
 
@@ -129,12 +156,12 @@ object TransferToHFile extends SparkApp {
     val conn = ConnectionFactory.createConnection(hbaseConf)
     val numOfRegionServers = conn.getAdmin.getClusterStatus.getServersSize
     val table = new HTable(hbaseConf, tableName)
-
+    val fs = FileSystem.get(hbaseConf)
 
     try {
 
       val rdd = sc.textFile(input)
-      val cells = buildCells(rdd, dbUrl, numOfRegionServers * maxHFilePerResionServer)
+      val cells = buildCells(rdd, dbUrl, hbaseConf, table, maxHFilePerResionServer)
       //      def toKeyValue(row: Array[Byte], cf: Array[Byte], qualifier: Array[Byte], data: Array[Byte]): KeyValue = {
       //        val kv = new KeyValue(row, cf, qualifier, data)
       //        println(s"[row]: ${row.toList}")
@@ -145,7 +172,7 @@ object TransferToHFile extends SparkApp {
       //        kv
       //      }
       //      new HFileMapRDD[Array[Byte]](cells, toKeyValue)
-      //      .toHBaseBulk(tableName, Bytes.toString(Graph.edgeCf), maxHFilePerResionServer)(HBaseConfig(hbaseConf))
+      //        .toHBaseBulk(tableName, Bytes.toString(Graph.edgeCf), maxHFilePerResionServer)(HBaseConfig(hbaseConf))
 
       //      println("buildCellsFinished")
       //
@@ -160,8 +187,34 @@ object TransferToHFile extends SparkApp {
 
       cells.saveAsNewAPIHadoopFile(output, classOf[ImmutableBytesWritable], classOf[KeyValue], classOf[HFileOutputFormat2], job.getConfiguration())
 
-
+//      val rwx = new FsPermission("777")
+//
+//      def setRecursivePermission(path: Path): Unit = {
+//        val listFiles = fs.listStatus(path)
+//        listFiles foreach { f =>
+//          val p = f.getPath
+//          fs.setPermission(p, rwx)
+//          if (f.isDirectory && p.getName != "_tmp") {
+//            // create a "_tmp" folder that can be used for HFile splitting, so that we can
+//            // set permissions correctly. This is a workaround for unsecured HBase. It should not
+//            // be necessary for SecureBulkLoadEndpoint (see https://issues.apache.org/jira/browse/HBASE-8495
+//            // and http://comments.gmane.org/gmane.comp.java.hadoop.hbase.user/44273)
+//            FileSystem.mkdirs(fs, new Path(p, "_tmp"), rwx)
+//            setRecursivePermission(p)
+//          }
+//        }
+//      }
+//      setRecursivePermission(new Path(output))
+//
+//      val lih = new LoadIncrementalHFiles(hbaseConf)
+//      lih.doBulkLoad(new Path(output), table)
     } finally {
+
+//      fs.deleteOnExit(output)
+
+      // clean HFileOutputFormat2 stuff
+//      fs.deleteOnExit(new Path(TotalOrderPartitioner.getPartitionFile(job.getConfiguration)))
+
       table.close()
       conn.close()
     }
