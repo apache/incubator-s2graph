eda4c869fe2a1225d53f3fb79141d11cf4ac2351
daewon <blueiur@gmail.com>
2015-10-05 17:19:58 +0900

    trivial refactoring

--- s2core/src/main/scala/com/kakao/s2graph/core/types/LabelWithDirection.scala
+++ s2core/src/main/scala/com/kakao/s2graph/core/types/LabelWithDirection.scala
@@ -7,7 +7,9 @@ import org.apache.hadoop.hbase.util.Bytes
  * Created by shon on 6/6/15.
  */
 object LabelWithDirection {
+
   import HBaseType._
+
   def apply(compositeInt: Int): LabelWithDirection = {
     //      logger.debug(s"CompositeInt: $compositeInt")
 
@@ -15,11 +17,13 @@ object LabelWithDirection {
     val labelId = compositeInt >> bitsForDir
     LabelWithDirection(labelId, dir)
   }
+
   def labelOrderSeqWithIsInverted(labelOrderSeq: Byte, isInverted: Boolean): Array[Byte] = {
     assert(labelOrderSeq < (1 << 6))
     val byte = labelOrderSeq << 1 | (if (isInverted) 1 else 0)
     Array.fill(1)(byte.toByte)
   }
+
   def bytesToLabelIndexSeqWithIsInverted(bytes: Array[Byte], offset: Int): (Byte, Boolean) = {
     val byte = bytes(offset)
     val isInverted = if ((byte & 1) != 0) true else false
@@ -27,21 +31,30 @@ object LabelWithDirection {
     (labelOrderSeq.toByte, isInverted)
   }
 }
+
 case class LabelWithDirection(labelId: Int, dir: Int) extends HBaseSerializable {
+
   import HBaseType._
+
   assert(dir < (1 << bitsForDir))
   assert(labelId < (Int.MaxValue >> bitsForDir))
 
   lazy val labelBits = labelId << bitsForDir
 
   lazy val compositeInt = labelBits | dir
+
   def bytes = Bytes.toBytes(compositeInt)
+
   lazy val dirToggled = LabelWithDirection(labelId, GraphUtil.toggleDir(dir))
+
   def updateDir(newDir: Int) = LabelWithDirection(labelId, newDir)
+
+  def isDirected = dir != 2
 
   override def hashCode(): Int = {
     compositeInt
   }
+
   override def equals(other: Any): Boolean = {
     other match {
       case o: LabelWithDirection => hashCode == o.hashCode()
