ec7d58d1824a96ae26a8d896fc19cb10aab6f102
SteamShon <shom83@gmail.com>
2015-10-29 17:16:11 +0900

    move package for logger.

--- s2core/src/main/scala/com/kakao/s2graph/core/Util.scala
+++ s2core/src/main/scala/com/kakao/s2graph/core/Util.scala
@@ -1,8 +1,15 @@
 package com.kakao.s2graph
 
+
+import com.stumbleupon.async.{Callback, Deferred}
 import play.api.Logger
 import play.api.libs.json.JsValue
 
+import scala.concurrent.{Promise, Future}
+
+/**
+ * Created by shon on 10/29/15.
+ */
 package object logger {
 
   trait Loggable[T] {
@@ -35,5 +42,52 @@ package object logger {
   def error[T: Loggable](msg: => T) = errorLogger.error(implicitly[Loggable[T]].toLogMessage(msg))
 }
 
+package object DeferOp {
 
+  def deferredToFuture[A](d: Deferred[A])(fallback: A): Future[A] = {
+    val promise = Promise[A]
 
+    d.addBoth(new Callback[Unit, A] {
+      def call(arg: A) = arg match {
+        case e: Exception =>
+          logger.error(s"deferred failed with return fallback: $e", e)
+          promise.success(fallback)
+        case _ => promise.success(arg)
+      }
+    })
+
+    promise.future
+  }
+
+  def deferredToFutureWithoutFallback[T](d: Deferred[T]) = {
+    val promise = Promise[T]
+
+    d.addBoth(new Callback[Unit, T] {
+      def call(arg: T) = arg match {
+        case e: Exception =>
+          logger.error(s"deferred return Exception: $e", e)
+          promise.failure(e)
+        case _ => promise.success(arg)
+      }
+    })
+
+    promise.future
+  }
+
+  def deferredCallbackWithFallback[T, R](d: Deferred[T])(f: T => R, fallback: => R) = {
+    d.addCallback(new Callback[R, T] {
+      def call(args: T): R = {
+        f(args)
+      }
+    }).addErrback(new Callback[R, Exception] {
+      def call(e: Exception): R = {
+        logger.error(s"Exception on deferred: $e", e)
+        fallback
+      }
+    })
+  }
+
+  def errorBack(block: => Exception => Unit) = new Callback[Unit, Exception] {
+    def call(ex: Exception): Unit = block(ex)
+  }
+}
