5e035f5de0a432fba3c2ba7173b3cef0ca24f8b2
emeth <mskim.org@gmail.com>
2015-11-10 11:26:05 +0900

    using join for filtering

--- s2ml/src/main/scala/com/kakao/ml/io/SourceFilter.scala
+++ s2ml/src/main/scala/com/kakao/ml/io/SourceFilter.scala
@@ -9,10 +9,13 @@ case class SourceFilterParams(
     userLowerBound: Option[Long],
     userUpperBound: Option[Long],
     itemLowerBound: Option[Long],
-    itemUpperBound: Option[Long]) extends Params
+    itemUpperBound: Option[Long],
+    strategy: Option[String]) extends Params
 
 class SourceFilter(params: SourceFilterParams)
     extends BaseDataProcessor[SourceData, SourceData](params) {
+
+  val defaultStrategy = "default"
 
   override def processBlock(sqlContext: SQLContext, input: SourceData): SourceData = {
 
@@ -20,11 +23,13 @@ class SourceFilter(params: SourceFilterParams)
     val userUpperBound = math.max(userLowerBound, params.userUpperBound.getOrElse(Long.MaxValue))
     val itemLowerBound = math.max(2, params.itemLowerBound.getOrElse(0L))
     val itemUpperBound = math.max(itemLowerBound, params.itemUpperBound.getOrElse(Long.MaxValue))
+    val strategy = params.strategy.getOrElse(defaultStrategy)
 
-    logInfo(s"userLowerBound: $userLowerBound")
-    logInfo(s"userUpperBound: $userUpperBound")
-    logInfo(s"itemLowerBound: $itemLowerBound")
-    logInfo(s"itemUpperBound: $itemUpperBound")
+    show(s"userLowerBound: $userLowerBound")
+    show(s"userUpperBound: $userUpperBound")
+    show(s"itemLowerBound: $itemLowerBound")
+    show(s"itemUpperBound: $itemUpperBound")
+    show(s"strategy: $strategy")
 
     /**
      * cube(userCol, itemCol).count() generates
@@ -37,29 +42,54 @@ class SourceFilter(params: SourceFilterParams)
         .cube(userCol, itemCol)
         .count()
 
-    /**
-     * this requires sufficient driver memory to store all userString and itemString.
-     */
-    val userItemCount = cubeCount
-        .where((userCol.isNotNull and itemCol.isNull and countCol.between(userLowerBound, userUpperBound))
-            or (itemCol.isNotNull and userCol.isNull and countCol.between(itemLowerBound, itemUpperBound)))
-        .select(userCol, itemCol, countCol)
-        .collect()
+    strategy match {
+      case `defaultStrategy` | "whitelist" =>
+        /**
+         * this requires sufficient driver memory to store all userString and itemString.
+         */
+        val userItemCount = cubeCount
+            .where((userCol.isNotNull and itemCol.isNull and countCol.between(userLowerBound, userUpperBound))
+                or (itemCol.isNotNull and userCol.isNull and countCol.between(itemLowerBound, itemUpperBound)))
+            .select(userCol, itemCol, countCol)
+            .collect()
 
-    val userActivities = userItemCount.filter(_.isNullAt(1)).map(x => x.getString(0) -> x.getLong(2).toInt)
-    val itemActivities = userItemCount.filter(_.isNullAt(0)).map(x => x.getString(1) -> x.getLong(2).toInt)
+        val userActivities = userItemCount.filter(_.isNullAt(1)).map(x => x.getString(0) -> x.getLong(2).toInt)
+        val itemActivities = userItemCount.filter(_.isNullAt(0)).map(x => x.getString(1) -> x.getLong(2).toInt)
 
-    /** TODO: efficient? */
-    val newSourceDF = input.sourceDF
-        .where(userCol.isin(userActivities.map(_._1): _*) and itemCol.isin(itemActivities.map(_._1): _*))
+        /** TODO: efficient? */
+        val newSourceDF = input.sourceDF
+            .where(userCol.isin(userActivities.map(_._1): _*) and itemCol.isin(itemActivities.map(_._1): _*))
 
-    /** materialize */
-    newSourceDF.persist(StorageLevel.MEMORY_AND_DISK)
-    val (tsCount, tsFrom, tsTo) = newSourceDF.select(count(tsCol), min(tsCol), max(tsCol)).head(1).map { row =>
-      (row.getLong(0), row.getLong(1), row.getLong(2))
-    }.head
+        /** materialize */
+        newSourceDF.persist(StorageLevel.MEMORY_AND_DISK)
+        val (tsCount, tsFrom, tsTo) = newSourceDF.select(count(tsCol), min(tsCol), max(tsCol)).head(1).map { row =>
+          (row.getLong(0), row.getLong(1), row.getLong(2))
+        }.head
 
-    SourceData(newSourceDF, tsCount, tsFrom, tsTo, input.labelWeight, Some(userActivities), Some(itemActivities))
+        SourceData(newSourceDF, tsCount, tsFrom, tsTo, input.labelWeight, Some(userActivities), Some(itemActivities))
+
+      case "join" =>
+        val userItemCount = cubeCount
+            .where((userCol.isNotNull and itemCol.isNull and countCol.between(userLowerBound, userUpperBound))
+                or (itemCol.isNotNull and userCol.isNull and countCol.between(itemLowerBound, itemUpperBound)))
+            .select(userCol, itemCol, countCol)
+
+        val userWhitelist = userItemCount.where(itemCol isNull).select(userCol)
+        val itemWhitelist = userItemCount.where(userCol isNull).select(itemCol)
+
+        val newSourceDF = input.sourceDF
+            .join(userWhitelist, userColString)
+            .join(itemWhitelist, itemColString)
+            .select(tsCol, labelCol, userCol, itemCol)
+
+        /** materialize */
+        newSourceDF.persist(StorageLevel.MEMORY_AND_DISK)
+        val (tsCount, tsFrom, tsTo) = newSourceDF.select(count(tsCol), min(tsCol), max(tsCol)).head(1).map { row =>
+          (row.getLong(0), row.getLong(1), row.getLong(2))
+        }.head
+
+        SourceData(newSourceDF, tsCount, tsFrom, tsTo, input.labelWeight, None, None)
+    }
   }
 
 }
