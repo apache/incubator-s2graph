fcfa38a79500f9b94cf881a512dc47e007a74528
SteamShon <shom83@gmail.com>
2015-11-23 19:02:24 +0900

    rearrange tc for murmur hash.

--- test/benchmark/GraphUtilSpec.scala
+++ test/benchmark/GraphUtilSpec.scala
@@ -5,51 +5,38 @@ import com.kakao.s2graph.core.types.{SourceVertexId, HBaseType, InnerVal, Vertex
 import org.apache.hadoop.hbase.util.Bytes
 import play.api.test.{FakeApplication, PlaySpecification}
 
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
+import scala.util.Random
 
 class GraphUtilSpec extends BenchmarkCommon with PlaySpecification {
 
 
   "GraphUtil" should {
-    "test murmur hash skew" in {
-      running(FakeApplication()) {
-        import HBaseType._
-        val testNum = 1000000L
-        val regionCount = 40
-        val window = Int.MaxValue / regionCount
-        val rangeShort = new ListBuffer[(Short, Short)]()
-
-        for {
-          i <- (0 until regionCount)
-        } yield {
-            val startKey =  Bytes.toBytes(i * window)
-            val endKey = Bytes.toBytes((i + 1) * window)
-            val x = Bytes.toShort(startKey.take(2))
-            val y = Bytes.toShort(endKey.take(2))
-            rangeShort += (x -> y)
-          }
-
-        def betweenShort(value: Short, start: Short, end: Short): Boolean =
-          start <= value && value <= end
-
-        val stats = new collection.mutable.HashMap[Int, Long]()
-
-        for (i <- (0L until testNum)) {
-          val hash = GraphUtil.murmur3(i.toString)
-          val headOpt = rangeShort.zipWithIndex.dropWhile { case ((start, end), idx) =>
-            !betweenShort(hash, start, end)
-          }.headOption
-          val head = headOpt match {
-            case None => rangeShort.zipWithIndex.last
-            case Some(s) => s
-          }
-          val key = head._2
-          val value = stats.getOrElse(key, 0L) + 1L
-          stats += (key -> value)
-        }
-        val result = stats.toList.sortBy(kv => kv._2).reverse
-        println(s"$result\n")
+    "test murmur3 hash function distribution" in {
+      val testNum = 1000000
+      val bucketSize = Short.MaxValue / 40
+      val countsNew = new mutable.HashMap[Int, Int]()
+      val counts = new mutable.HashMap[Int, Int]()
+      for {
+        i <- (0 until testNum)
+      } {
+        val h = GraphUtil.murmur3(i.toString) / bucketSize
+        val hNew = GraphUtil.murmur3Int(i.toString) / bucketSize
+        counts += (h -> (counts.getOrElse(h, 0) + 1))
+        countsNew += (hNew -> (countsNew.getOrElse(hNew, 0) + 1))
       }
+      val all = counts.toList.sortBy { case (bucket, count) => count }.reverse
+      val allNew = countsNew.toList.sortBy { case (bucket, count) => count }.reverse
+      val top = all.take(10)
+      val bottom = all.takeRight(10)
+      val topNew = allNew.take(10)
+      val bottomNew = allNew.takeRight(10)
+      println(s"Top: $top")
+      println(s"Bottom: $bottom")
+      println("-" * 50)
+      println(s"TopNew: $topNew")
+      println(s"Bottom: $bottomNew")
       true
     }
 
@@ -65,8 +52,6 @@ class GraphUtilSpec extends BenchmarkCommon with PlaySpecification {
         } yield {
           val startKey =  Bytes.toBytes(i * window)
           val endKey = Bytes.toBytes((i + 1) * window)
-          val x = Bytes.toShort(startKey.take(2))
-          val y = Bytes.toShort(endKey.take(2))
           rangeBytes += (startKey.toList -> endKey.toList)
         }
 
@@ -76,28 +61,41 @@ class GraphUtilSpec extends BenchmarkCommon with PlaySpecification {
         def between(bytes: Array[Byte], startKey: Array[Byte], endKey: Array[Byte]): Boolean =
           (Bytes.compareTo(startKey, bytes) >= 0 && Bytes.compareTo(bytes, endKey) <= 0)
 
-        val stats = new collection.mutable.HashMap[Int, Long]()
+        val stats = new collection.mutable.HashMap[Int, ((List[Byte], List[Byte]), Long)]()
+        val counts = new collection.mutable.HashMap[Short, Long]()
+        stats += (0 -> (rangeBytes.head -> 0L))
 
         for (i <- (0L until testNum)) {
           val vertexId = SourceVertexId(DEFAULT_COL_ID, InnerVal.withLong(i, HBaseType.DEFAULT_VERSION))
           val bytes = vertexId.bytes
-          val headOpt = rangeBytes.zipWithIndex.dropWhile { case ((start, end), idx) =>
-            !between(bytes, start.toArray, end.toArray)
-          }.headOption
-          val head = headOpt match {
-            case None => rangeBytes.zipWithIndex.last
-            case Some(s) => s
+          val shortKey = Bytes.toShort(bytes.take(2))
+          val shortVal = counts.getOrElse(shortKey, 0L) + 1L
+          counts += (shortKey -> shortVal)
+
+          var j = 0
+          var found = false
+          while (j < rangeBytes.size && !found) {
+            val (start, end) = rangeBytes(j)
+            if (between(bytes, start.toArray, end.toArray)) {
+              found = true
+            }
+            j += 1
           }
-          val key = head._2
-          val value = stats.getOrElse(key, 0L) + 1L
-          stats += (key -> value)
+          val head = rangeBytes(j - 1)
+          val key = j - 1
+          val value = stats.get(key) match {
+            case None => 0L
+            case Some(v) => v._2 + 1
+          }
+          stats += (key -> (head, value))
         }
-        val result = stats.toList.sortBy(kv => kv._2).reverse
-        //        val result2 = stats2.toList.sortBy(kv => kv._2).reverse.take(100)
-        //        val result3 = stats3.toList.sortBy(kv => kv._2).reverse.take(100)
-        println(s"$result\n")
-        //        println(s"$result2\n")
-        //        println(s"$result3\n")
+        stats.toList.sortBy(kv => kv._2._2).reverse.foreach { case (idx, ((start, end), cnt)) =>
+          val startShort = Bytes.toShort(start.take(2).toArray)
+          val endShort = Bytes.toShort(end.take(2).toArray)
+          val count = counts.count(t => startShort <= t._1 && t._1 < endShort)
+          println(s"$idx: $start ~ $end\t$startShort ~ $endShort\t$cnt\t$count")
+
+        }
       }
       true
     }
