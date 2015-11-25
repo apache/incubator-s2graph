2514e46a1a5d06b464ee793958669777847b847c
emeth <mskim.org@gmail.com>
2015-11-25 21:58:04 +0900

    modify wallogtohdfs

--- loader/src/main/scala/subscriber/WalLogToHDFS.scala
+++ loader/src/main/scala/subscriber/WalLogToHDFS.scala
@@ -47,8 +47,25 @@ object WalLogToHDFS extends SparkApp with WithKafka {
 
     val mapAcc = sc.accumulable(new MutableHashMap[String, Long](), "Throughput")(HashMapParam[String, Long](_ + _))
 
-    var splits = Array("all")
+    val hdfsBlockSize = 134217728 // 128M
+    val hiveContext = new HiveContext(sc)
+    var splits = Array[String]()
+    var excludeLabels = Set[String]()
+    var excludeServices = Set[String]()
     stream.foreachRDD { (rdd, time) =>
+      try {
+        val read = sc.textFile(splitListPath).collect().map(_.split("=")).flatMap {
+          case Array(value) => Some(("split", value))
+          case Array(key, value) => Some((key, value))
+          case _ => None
+        }
+        splits = read.filter(_._1 == "split").map(_._2)
+        excludeLabels = read.filter(_._1 == "exclude_label").map(_._2).toSet
+        excludeServices = read.filter(_._1 == "exclude_service").map(_._2).toSet
+      } catch {
+        case _: Throwable => // use previous information
+      }
+
       val offsets = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
 
       val elements = rdd.mapPartitions { partition =>
@@ -58,12 +75,18 @@ object WalLogToHDFS extends SparkApp with WithKafka {
 
         partition.flatMap { case (key, msg) =>
           val optMsg = Graph.toGraphElement(msg).flatMap { element =>
-            val n = msg.split("\t", 7).length
-            if(n == 6) {
-              Some(Seq(msg, "{}", element.serviceName).mkString("\t"))
+            val arr = msg.split("\t", 7)
+            val service = element.serviceName
+            val label = arr(5)
+            val n = arr.length
+
+            if (excludeServices.contains(service) || excludeLabels.contains(label)) {
+              None
+            } else if(n == 6) {
+              Some(Seq(msg, "{}", service).mkString("\t"))
             }
             else if(n == 7) {
-              Some(Seq(msg, element.serviceName).mkString("\t"))
+              Some(Seq(msg, service).mkString("\t"))
             }
             else {
               None
@@ -73,21 +96,13 @@ object WalLogToHDFS extends SparkApp with WithKafka {
         }
       }
 
-      try {
-        // update splits
-        val read = sc.textFile(splitListPath).collect().map(_.trim)
-        if (read.length > 0) splits =  read
-      } catch {
-        case _: Throwable => // use previous splits
-      }
-
       val ts = time.milliseconds
       val dateId = new SimpleDateFormat("yyyy-MM-dd").format(new Date(ts))
 
       /** make sure that `elements` are not running at the same time */
       val elementsWritten = {
         elements.cache()
-        splits.foreach {
+        (Array("all") ++ splits).foreach {
           case split if split == "all" =>
             val path = s"$outputPath/split=$split/date_id=$dateId/ts=$ts"
             elements.saveAsTextFile(path)
@@ -95,8 +110,15 @@ object WalLogToHDFS extends SparkApp with WithKafka {
             val path = s"$outputPath/split=$split/date_id=$dateId/ts=$ts"
             val strlen = split.length
             val splitData = elements.filter(_.takeRight(strlen) == split).cache()
-            val numPartitions = math.max(1, (splitData.count() / 5e5).toInt)
-            splitData.repartition(numPartitions).saveAsTextFile(path)
+            val totalSize = splitData
+                .mapPartitions { iterator =>
+                  val s = iterator.map(_.length.toLong).sum
+                  Iterator.single(s)
+                }
+                .sum
+                .toLong
+            val numPartitions = math.max(1, (totalSize / hdfsBlockSize.toDouble).toInt)
+            splitData.coalesce(math.min(splitData.partitions.length, numPartitions)).saveAsTextFile(path)
             splitData.unpersist()
         }
         elements.unpersist()
@@ -112,7 +134,6 @@ object WalLogToHDFS extends SparkApp with WithKafka {
         (_: Nothing) => ()
       }
 
-      val hiveContext = new HiveContext(sc)
       splits.foreach { split =>
         val path = s"$outputPath/split=$split/date_id=$dateId/ts=$ts"
         hiveContext.sql(s"use $hiveDatabase")
