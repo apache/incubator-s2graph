b7b25344275e3a13a1e9ae770deedf73f3628832
daewon <blueiur@gmail.com>
2015-12-07 16:32:41 +0900

    add Shutdown hook

--- s2rest_finagle/src/main/scala/Server.scala
+++ s2rest_finagle/src/main/scala/Server.scala
@@ -4,7 +4,10 @@ import java.util.concurrent.Executors
 
 import com.kakao.s2graph.core._
 import com.kakao.s2graph.core.utils.logger
+import com.twitter
 import com.twitter.finagle
+import com.twitter.finagle.{Service, _}
+import com.twitter.util._
 import com.typesafe.config.ConfigFactory
 import play.api.libs.json.Json
 
@@ -12,10 +15,6 @@ import scala.concurrent.ExecutionContext
 import scala.util.Success
 
 object FinagleServer extends App {
-
-  import com.twitter.finagle.{Service, _}
-  import com.twitter.util._
-
   val numOfThread = Runtime.getRuntime.availableProcessors()
   val threadPool = Executors.newFixedThreadPool(numOfThread)
   implicit val ec = ExecutionContext.fromExecutor(threadPool)
@@ -25,12 +24,13 @@ object FinagleServer extends App {
   // init s2graph with config
   val s2graph = new Graph(config)(ec)
   val s2parser = new RequestParser(s2graph)
-  var isHealthy = true
 
+  // app status code
+  var isHealthy = true
 
   val service = new Service[http.Request, http.Response] {
     def apply(req: http.Request): Future[http.Response] = {
-      val promise = new com.twitter.util.Promise[http.Response]
+      val promise = new twitter.util.Promise[http.Response]
       val startedAt = System.currentTimeMillis()
 
       req.method match {
@@ -75,7 +75,7 @@ object FinagleServer extends App {
               promise.become(Future.value(httpRes))
           }
 
-         case finagle.http.Method.Put =>
+        case finagle.http.Method.Put =>
           req.path match {
             case path if path.startsWith("/health_check") =>
               val op = path.split("/").last.toBoolean
@@ -95,8 +95,18 @@ object FinagleServer extends App {
     }
   }
 
-  val port = try config.getInt("http.port") catch { case e: Exception => 9000 }
+  val port = try config.getInt("http.port") catch {
+    case e: Exception => 9000
+  }
+
+
   val server = Http.serve(s":$port", FinagleServer.service)
+  Runtime.getRuntime().addShutdownHook(new Thread() {
+    override def run(): Unit = {
+      s2graph.shutdown()
+      Await.ready(server.close())
+    }
+  })
 
   Await.ready(server)
 }
