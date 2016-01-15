58e424678e7f83af751b186f4fe2d6717e55507d
DO YUNG YOON <steamshon@apache.org>
2016-01-16 07:35:46 +0900

    [S2GRAPH-25] comment out test case to check secured hbase cluster.

--- s2core/src/test/scala/com/kakao/s2graph/core/storage/hbase/AsynchbaseStorageTest.scala
+++ s2core/src/test/scala/com/kakao/s2graph/core/storage/hbase/AsynchbaseStorageTest.scala
@@ -7,31 +7,30 @@ import scala.collection.JavaConversions._
 
 class AsynchbaseStorageTest extends FunSuite with Matchers {
 
-  test("test secure cluster connection") {
-    val config = ConfigFactory.parseMap(
-      Map(
-        "hbase.zookeeper.quorum" -> "localhost",
-        "hbase.security.auth.enable" -> "true",
-        "hbase.security.authentication" -> "kerberos",
-        "hbase.kerberos.regionserver.principal" -> "hbase/_HOST@LOCAL.HBASE",
-        "hbase.sasl.clientconfig" -> "Client",
-        "java.security.krb5.conf" -> "krb5.conf",
-        "java.security.auth.login.config" -> "async-client.jaas.conf")
-    )
-
-    val client = AsynchbaseStorage.makeClient(config)
-    Thread.sleep(10000)
-    println("*\n" * 50)
-    val table = "test".getBytes()
-
-    val putRequest = new PutRequest(table, "a".getBytes(), "e".getBytes, "a".getBytes, "a".getBytes)
-    val getRequest = new GetRequest(table, "a".getBytes(), "e".getBytes)
-    val ret = client.put(putRequest).join()
-    val kvs = client.get(getRequest).join()
-    for {
-      kv <- kvs
-    } {
-      println(kv.toString)
-    }
-  }
+  /** need secured cluster */
+//  test("test secure cluster connection") {
+//    val config = ConfigFactory.parseMap(
+//      Map(
+//        "hbase.zookeeper.quorum" -> "localhost",
+//        "hbase.security.auth.enable" -> "true",
+//        "hbase.security.authentication" -> "kerberos",
+//        "hbase.kerberos.regionserver.principal" -> "hbase/_HOST@LOCAL.HBASE",
+//        "hbase.sasl.clientconfig" -> "Client",
+//        "java.security.krb5.conf" -> "krb5.conf",
+//        "java.security.auth.login.config" -> "async-client.jaas.conf")
+//    )
+//
+//    val client = AsynchbaseStorage.makeClient(config)
+//    val table = "test".getBytes()
+//
+//    val putRequest = new PutRequest(table, "a".getBytes(), "e".getBytes, "a".getBytes, "a".getBytes)
+//    val getRequest = new GetRequest(table, "a".getBytes(), "e".getBytes)
+//    val ret = client.put(putRequest).join()
+//    val kvs = client.get(getRequest).join()
+//    for {
+//      kv <- kvs
+//    } {
+//      println(kv.toString)
+//    }
+//  }
 }
