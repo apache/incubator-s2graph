838bd9043fee7c1e0f6200dfa998444de2a957b9
DO YUNG YOON <steamshon@apache.org>
2016-03-07 11:13:57 +0900

    add more label schemaVersions for test on IndexEdgeTest.

--- s2core/src/test/scala/com/kakao/s2graph/core/storage/hbase/IndexEdgeTest.scala
+++ s2core/src/test/scala/com/kakao/s2graph/core/storage/hbase/IndexEdgeTest.scala
@@ -1,6 +1,6 @@
 package com.kakao.s2graph.core.storage.hbase
 
-import com.kakao.s2graph.core.mysqls.{LabelMeta, LabelIndex}
+import com.kakao.s2graph.core.mysqls.{Label, LabelMeta, LabelIndex}
 import com.kakao.s2graph.core.{IndexEdge, Vertex, TestCommonWithModels}
 import com.kakao.s2graph.core.types._
 import org.scalatest.{FunSuite, Matchers}
@@ -9,71 +9,73 @@ import org.scalatest.{FunSuite, Matchers}
 class IndexEdgeTest extends FunSuite with Matchers with TestCommonWithModels {
   initTests()
 
-  /** note that props have to be properly set up for equals */
-  test("test serializer/deserializer for index edge.") {
-    val ts = System.currentTimeMillis()
-    val from = InnerVal.withLong(1, labelV4.schemaVersion)
-    val to = InnerVal.withLong(101, labelV4.schemaVersion)
+  /**
+   * check if storage serializer/deserializer can translate from/to bytes array.
+   * @param l: label for edge.
+   * @param ts: timestamp for edge.
+   * @param to: to VertexId for edge.
+   * @param props: expected props of edge.
+   */
+  def check(l: Label, ts: Long, to: InnerValLike, props: Map[Byte, InnerValLike]): Unit = {
+    val from = InnerVal.withLong(1, l.schemaVersion)
     val vertexId = SourceVertexId(HBaseType.DEFAULT_COL_ID, from)
     val tgtVertexId = TargetVertexId(HBaseType.DEFAULT_COL_ID, to)
     val vertex = Vertex(vertexId, ts)
     val tgtVertex = Vertex(tgtVertexId, ts)
-    val labelWithDir = LabelWithDirection(labelV4.id.get, 0)
+    val labelWithDir = LabelWithDirection(l.id.get, 0)
 
-    val tsInnerVal = InnerVal.withLong(ts, labelV4.schemaVersion)
-    val props = Map(LabelMeta.timeStampSeq -> tsInnerVal,
-      1.toByte -> InnerVal.withDouble(2.1, labelV4.schemaVersion))
     val indexEdge = IndexEdge(vertex, tgtVertex, labelWithDir, 0, ts, LabelIndex.DefaultSeq, props)
-    val _indexEdgeOpt = graph.storage.indexEdgeDeserializer(labelV4.schemaVersion).fromKeyValues(queryParam,
-      graph.storage.indexEdgeSerializer(indexEdge).toKeyValues, labelV4.schemaVersion, None)
+    val _indexEdgeOpt = graph.storage.indexEdgeDeserializer(l.schemaVersion).fromKeyValues(queryParam,
+      graph.storage.indexEdgeSerializer(indexEdge).toKeyValues, l.schemaVersion, None)
 
     _indexEdgeOpt should not be empty
     indexEdge should be(_indexEdgeOpt.get)
+  }
 
+
+  /** note that props have to be properly set up for equals */
+  test("test serializer/deserializer for index edge.") {
+    val ts = System.currentTimeMillis()
+    for {
+      l <- Seq(label, labelV2, labelV3, labelV4)
+    } {
+      val to = InnerVal.withLong(101, l.schemaVersion)
+      val tsInnerVal = InnerVal.withLong(ts, l.schemaVersion)
+      val props = Map(LabelMeta.timeStampSeq -> tsInnerVal,
+        1.toByte -> InnerVal.withDouble(2.1, l.schemaVersion))
+
+      check(l, ts, to, props)
+    }
   }
 
   test("test serializer/deserializer for degree edge.") {
     val ts = System.currentTimeMillis()
-    val from = InnerVal.withLong(1, labelV4.schemaVersion)
-    val to = InnerVal.withStr("0", labelV4.schemaVersion)
-    val vertexId = SourceVertexId(HBaseType.DEFAULT_COL_ID, from)
-    val tgtVertexId = TargetVertexId(HBaseType.DEFAULT_COL_ID, to)
-    val vertex = Vertex(vertexId, ts)
-    val tgtVertex = Vertex(tgtVertexId, ts)
-    val labelWithDir = LabelWithDirection(labelV4.id.get, 0)
+    for {
+      l <- Seq(label, labelV2, labelV3, labelV4)
+    } {
+      val to = InnerVal.withStr("0", l.schemaVersion)
+      val tsInnerVal = InnerVal.withLong(ts, l.schemaVersion)
+      val props = Map(
+        LabelMeta.degreeSeq -> InnerVal.withLong(10, l.schemaVersion),
+        LabelMeta.timeStampSeq -> tsInnerVal)
 
-    val tsInnerVal = InnerVal.withLong(ts, labelV4.schemaVersion)
-    val props = Map(
-      LabelMeta.degreeSeq -> InnerVal.withLong(10, labelV4.schemaVersion),
-      LabelMeta.timeStampSeq -> tsInnerVal)
-
-    val indexEdge = IndexEdge(vertex, tgtVertex, labelWithDir, 0, ts, LabelIndex.DefaultSeq, props)
-    val _indexEdgeOpt = graph.storage.indexEdgeDeserializer(labelV4.schemaVersion).fromKeyValues(queryParam,
-      graph.storage.indexEdgeSerializer(indexEdge).toKeyValues, labelV4.schemaVersion, None)
-
-    _indexEdgeOpt should not be empty
-    indexEdge should be(_indexEdgeOpt.get)
+      check(l, ts, to, props)
+    }
   }
 
   test("test serializer/deserializer for incrementCount index edge.") {
     val ts = System.currentTimeMillis()
-    val from = InnerVal.withLong(1, labelV4.schemaVersion)
-    val to = InnerVal.withLong(101, labelV4.schemaVersion)
-    val vertexId = SourceVertexId(HBaseType.DEFAULT_COL_ID, from)
-    val tgtVertexId = TargetVertexId(HBaseType.DEFAULT_COL_ID, to)
-    val vertex = Vertex(vertexId, ts)
-    val tgtVertex = Vertex(tgtVertexId, ts)
-    val labelWithDir = LabelWithDirection(labelV4.id.get, 0)
+    for {
+      l <- Seq(label, labelV2, labelV3, labelV4)
+    } {
+      val to = InnerVal.withLong(101, l.schemaVersion)
 
-    val tsInnerVal = InnerVal.withLong(ts, labelV4.schemaVersion)
-    val props = Map(LabelMeta.timeStampSeq -> tsInnerVal,
-      1.toByte -> InnerVal.withDouble(2.1, labelV4.schemaVersion),
-      LabelMeta.countSeq -> InnerVal.withLong(10, labelV4.schemaVersion))
+      val tsInnerVal = InnerVal.withLong(ts, l.schemaVersion)
+      val props = Map(LabelMeta.timeStampSeq -> tsInnerVal,
+        1.toByte -> InnerVal.withDouble(2.1, l.schemaVersion),
+        LabelMeta.countSeq -> InnerVal.withLong(10, l.schemaVersion))
 
-    val indexEdge = IndexEdge(vertex, tgtVertex, labelWithDir, 0, ts, LabelIndex.DefaultSeq, props)
-    val _indexEdgeOpt = graph.storage.indexEdgeDeserializer(labelV4.schemaVersion).fromKeyValues(queryParam,
-      graph.storage.indexEdgeSerializer(indexEdge).toKeyValues, labelV4.schemaVersion, None)
-    _indexEdgeOpt should not be empty
-    indexEdge should be(_indexEdgeOpt.get)
+      check(l, ts, to, props)
+    }
   }
 }
