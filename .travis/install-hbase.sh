69961be46d399f6932272d8b7b4dfad3aeef469b
Injun Song <ijsong@gmail.com>
2016-12-02 17:00:35 +0900

    fix

--- .travis/install-hbase.sh
+++ .travis/install-hbase.sh
@@ -2,7 +2,6 @@
 
 set -xe
 
-if [ ! -d "$HOME/hbase/bin" ]; then
+if [ ! -d "$HOME/hbase-$HBASE_VERSION/bin" ]; then
   cd $HOME && wget -q -O - http://mirror.navercorp.com/apache/hbase/stable/hbase-$HBASE_VERSION-bin.tar.gz | tar xz
-  ln -s $HOME/hbase-$HBASE_VERSION $HOME/hbase
 fi
