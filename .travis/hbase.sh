342db9f7782957ee53e79d3ff3a6e2164c46a5b4
Injun Song <ijsong@gmail.com>
2016-12-02 16:49:57 +0900

    fix

--- .travis/hbase.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget -q -O - http://mirror.navercorp.com/apache/hbase/stable/hbase-1.2.4-bin.tar.gz | tar xz
-cd hbase-1.2.4
-bin/start-hbase.sh
---
+++ .travis/install-hbase.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ ! -d $HOME/hbase/bin ]; then
+  cd $HOME && wget -q -O - http://mirror.navercorp.com/apache/hbase/stable/hbase-$HBASE_VERSION-bin.tar.gz | tar xz
+  ln -s $HOME/hbase-$HBASE_VERSION $HOME/hbase
+  $HOME/hbase/bin/start-hbase.sh
+fi
