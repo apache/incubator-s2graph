33ed1d8d5bbaa2eca46d49a2e6c042fb39754636
DO YUNG YOON <steamshon@apache.org>
2017-07-21 21:03:40 +0900

    run apache-rat.

--- s2core/src/main/scala/org/apache/s2graph/core/index/IndexProvider.scala
+++ s2core/src/main/scala/org/apache/s2graph/core/index/IndexProvider.scala
@@ -1,4 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.s2graph.core.index
+
+import java.util
 
 import com.typesafe.config.Config
 import org.apache.lucene.analysis.standard.StandardAnalyzer
@@ -14,6 +35,8 @@ import org.apache.s2graph.core.types.{InnerValLike, VertexId}
 import org.apache.s2graph.core.utils.logger
 import org.apache.tinkerpop.gremlin.process.traversal.step.util.HasContainer
 import play.api.libs.json.Json
+
+import scala.concurrent.Future
 
 object IndexProvider {
   val vidField = "_vid_"
@@ -40,12 +63,16 @@ object IndexProvider {
 trait IndexProvider {
   //TODO: Seq nee do be changed into stream
   def fetchEdgeIds(queryString: String): java.util.List[EdgeId]
+  def fetchEdgeIdsAsync(queryString: String): Future[java.util.List[EdgeId]]
 
   def fetchVertexIds(queryString: String): java.util.List[VertexId]
+  def fetchVertexIdsAsync(queryString: String): Future[java.util.List[VertexId]]
 
   def mutateVertices(vertices: Seq[S2Vertex]): Seq[Boolean]
+  def mutateVerticesAsync(vertices: Seq[S2Vertex]): Future[Seq[Boolean]]
 
   def mutateEdges(edges: Seq[S2Edge]): Seq[Boolean]
+  def mutateEdgesAsync(edges: Seq[S2Edge]): Future[Seq[Boolean]]
 
   def shutdown(): Unit
 }
@@ -144,4 +171,11 @@ class LuceneIndexProvider(config: Config) extends IndexProvider {
     writer.close()
   }
 
+  override def fetchEdgeIdsAsync(queryString: String): Future[util.List[EdgeId]] = Future.successful(fetchEdgeIds(queryString))
+
+  override def fetchVertexIdsAsync(queryString: String): Future[util.List[VertexId]] = Future.successful(fetchVertexIds(queryString))
+
+  override def mutateVerticesAsync(vertices: Seq[S2Vertex]): Future[Seq[Boolean]] = Future.successful(mutateVertices(vertices))
+
+  override def mutateEdgesAsync(edges: Seq[S2Edge]): Future[Seq[Boolean]] = Future.successful(mutateEdges(edges))
 }
